ac5fdbf83251bf1dfc6cb081fefb24354524920f
Richard Dalrymple <richarddalrymple@Richards-MacBook-Pro.local>
2023-05-06 17:50:31 +0100

    fourth commit

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -5,7 +5,9 @@ const Pokemon = ({pokemon}) => {
     return ( 
         <div className='pokemon'>
         <li>{pokemon.name}</li>
+        <div className='pokemon-image'>
         <img src={pokemon.sprites.front_shiny}></img>
+        </div>
         <li>Type: {pokemon['types'][0]['type']['name']}</li>
             <div className='move'>
                 Top Moves: <li>{pokemon['moves'][0]['move']['name']}</li>
